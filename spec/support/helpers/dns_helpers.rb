945da7105d99191abb22371049110345381abc4a
Sean McGivern <sean@gitlab.com>
2021-01-07 19:45:37 +0000

    Set timeout for DNS resolution in UrlBlocker
    If DNS resolution takes a very long time, an HTTP request can exceed its
    time limit - in some cases significantly.
    
    Ruby 2.7 adds a `timeout` keyword argument to `Addrinfo.getaddrinfo`. We
    set that to the remaining time for the current request (if we're in a
    request), bounded by zero on the lower end (so we don't try to set a
    negative timeout), and five seconds on the upper end (as a reasonable
    maximum).
    
    This `timeout` argument only works on systems with `getaddrinfo_a`
    available.
    
    If the timeout is hit, `Addrinfo.getaddrinfo` raises a `SocketError`,
    which is already handled correctly.

--- spec/support/helpers/dns_helpers.rb
+++ spec/support/helpers/dns_helpers.rb
@@ -12,19 +12,18 @@ module DnsHelpers
   end
 
   def stub_all_dns!
-    allow(Addrinfo).to receive(:getaddrinfo).with(anything, anything, nil, :STREAM).and_return([])
+    allow(Addrinfo).to receive(:getaddrinfo).with(anything, anything, nil, :STREAM, anything).and_return([])
     allow(Addrinfo).to receive(:getaddrinfo).with(anything, anything, nil, :STREAM, anything, anything).and_return([])
   end
 
   def stub_invalid_dns!
-    allow(Addrinfo).to receive(:getaddrinfo).with(/\Afoobar\.\w|(\d{1,3}\.){4,}\d{1,3}\z/i, anything, nil, :STREAM) do
+    allow(Addrinfo).to receive(:getaddrinfo).with(/\Afoobar\.\w|(\d{1,3}\.){4,}\d{1,3}\z/i, anything, nil, :STREAM, anything) do
       raise SocketError.new("getaddrinfo: Name or service not known")
     end
   end
 
   def permit_local_dns!
     local_addresses = /\A(127|10)\.0\.0\.\d{1,3}|(192\.168|172\.16)\.\d{1,3}\.\d{1,3}|0\.0\.0\.0|localhost\z/i
-    allow(Addrinfo).to receive(:getaddrinfo).with(local_addresses, anything, nil, :STREAM).and_call_original
-    allow(Addrinfo).to receive(:getaddrinfo).with(local_addresses, anything, nil, :STREAM, anything, anything, any_args).and_call_original
+    allow(Addrinfo).to receive(:getaddrinfo).with(local_addresses, anything, nil, :STREAM, any_args).and_call_original
   end
 end
