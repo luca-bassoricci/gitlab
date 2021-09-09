8a41de56fe82abbedfdf1bedd041797f434850a3
drew cimino <dcimino@gitlab.com>
2021-09-09 13:39:45 -0400

    Improve StuckCiJobsWorker spec
    Move exclusive lease specs into method context
    Add DropService execution spec

--- spec/workers/stuck_ci_jobs_worker_spec.rb
+++ spec/workers/stuck_ci_jobs_worker_spec.rb
@@ -5,8 +5,9 @@ require 'spec_helper'
 RSpec.describe StuckCiJobsWorker do
   include ExclusiveLeaseHelpers
 
-  let(:worker_lease_key) { StuckCiJobsWorker::EXCLUSIVE_LEASE_KEY }
+  let(:worker_lease_key)  { StuckCiJobsWorker::EXCLUSIVE_LEASE_KEY }
   let(:worker_lease_uuid) { SecureRandom.uuid }
+  let(:worker2)           { described_class.new }
 
   subject(:worker) { described_class.new }
 
@@ -14,32 +15,40 @@ RSpec.describe StuckCiJobsWorker do
     stub_exclusive_lease(worker_lease_key, worker_lease_uuid)
   end
 
-  describe 'exclusive lease' do
-    let(:worker2) { described_class.new }
-
-    it 'is guard by exclusive lease when executed concurrently' do
-      expect(worker).to receive(:remove_lease).exactly(:once)
-      expect(worker2).not_to receive(:remove_lease)
+  describe '#perform' do
+    it 'executes an instance of Ci::StuckBuildsDropService' do
+      expect_next_instance_of(Ci::StuckBuilds::DropService) do |service|
+        expect(service).to receive(:execute).exactly(:once)
+      end
 
       worker.perform
-
-      stub_exclusive_lease_taken(worker_lease_key)
-
-      worker2.perform
     end
 
-    it 'can be executed in sequence' do
-      expect(worker).to receive(:remove_lease).at_least(:once)
-      expect(worker2).to receive(:remove_lease).at_least(:once)
+    context 'with an exclusive lease' do
+      it 'does not execute concurrently' do
+        expect(worker).to receive(:remove_lease).exactly(:once)
+        expect(worker2).not_to receive(:remove_lease)
 
-      worker.perform
-      worker2.perform
-    end
+        worker.perform
 
-    it 'cancels exclusive leases after worker perform' do
-      expect_to_cancel_exclusive_lease(worker_lease_key, worker_lease_uuid)
+        stub_exclusive_lease_taken(worker_lease_key)
 
-      worker.perform
+        worker2.perform
+      end
+
+      it 'can execute in sequence' do
+        expect(worker).to receive(:remove_lease).at_least(:once)
+        expect(worker2).to receive(:remove_lease).at_least(:once)
+
+        worker.perform
+        worker2.perform
+      end
+
+      it 'cancels exclusive leases after worker perform' do
+        expect_to_cancel_exclusive_lease(worker_lease_key, worker_lease_uuid)
+
+        worker.perform
+      end
     end
   end
 end
