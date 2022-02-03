d08b872442657b35075e3c3a278ed21dd5c994ed
Brett Walker <bwalker@gitlab.com>
2022-02-03 16:46:45 -0600

    Add connection parameter to define_batchable_model

--- lib/gitlab/background_migration/backfill_member_namespace_for_group_members.rb
+++ lib/gitlab/background_migration/backfill_member_namespace_for_group_members.rb
@@ -26,7 +26,7 @@ module Gitlab
       private
 
       def relation_scoped_to_range(source_table, source_key_column, start_id, stop_id)
-        define_batchable_model(source_table)
+        define_batchable_model(source_table, connection: ActiveRecord::Base.connection)
           .where(source_key_column => start_id..stop_id)
           .where(type: 'GroupMember')
           .where(member_namespace_id: nil)
