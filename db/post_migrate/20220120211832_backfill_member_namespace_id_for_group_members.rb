0349fbe0864899001d3bb41e0964d231c2f6be13
charlie ablett <cablett@gitlab.com>
2022-02-17 23:10:21 +1300

    Migrate MAINTAINER access memberships to OWNER
    - For personal namespace projects only
    - Only for the "owner" of the namespace
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/80146

--- db/post_migrate/20220120211832_backfill_member_namespace_id_for_group_members.rb
+++ db/post_migrate/20220120211832_backfill_member_namespace_id_for_group_members.rb
@@ -4,8 +4,8 @@ class BackfillMemberNamespaceIdForGroupMembers < Gitlab::Database::Migration[1.0
   MIGRATION = 'BackfillMemberNamespaceForGroupMembers'
   INTERVAL = 2.minutes
   BATCH_SIZE = 1_000
-  MAX_BATCH_SIZE = 10_000
-  SUB_BATCH_SIZE = 200
+  MAX_BATCH_SIZE = 2_000
+  SUB_BATCH_SIZE = 100
 
   def up
     queue_batched_background_migration(
@@ -15,6 +15,7 @@ class BackfillMemberNamespaceIdForGroupMembers < Gitlab::Database::Migration[1.0
       job_interval: INTERVAL,
       batch_size: BATCH_SIZE,
       max_batch_size: MAX_BATCH_SIZE,
+      max_batch_value: 6_000_000, #5_700_754,
       sub_batch_size: SUB_BATCH_SIZE
     )
   end
