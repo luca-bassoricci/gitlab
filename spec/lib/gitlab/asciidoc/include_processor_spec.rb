bf0ee927fcdb4923ebee6173b488d7b664fa457e
Michael Le <mle@gitlab.com>
2022-06-08 21:59:19 +1000

    Add test for partial lines

--- spec/lib/gitlab/asciidoc/include_processor_spec.rb
+++ spec/lib/gitlab/asciidoc/include_processor_spec.rb
@@ -18,7 +18,10 @@ RSpec.describe Gitlab::Asciidoc::IncludeProcessor do
   let(:max_includes) { 10 }
 
   let(:reader) { Asciidoctor::PreprocessorReader.new(document, lines, 'file.adoc') }
+  let(:reader_lines) { Asciidoctor::PreprocessorReader.new(partial_document, partial_lines, 'file.adoc') }
+
   let(:document) { Asciidoctor::Document.new(lines) }
+  let(:partial_document) { Asciidoctor::Document.new(partial_lines) }
 
   subject(:processor) { described_class.new(processor_context) }
 
@@ -26,6 +29,7 @@ RSpec.describe Gitlab::Asciidoc::IncludeProcessor do
   let(:a_data) { StringIO.new('include::b.adoc[]') }
 
   let(:lines) { [':max-include-depth: 1000'] + Array.new(10, 'include::a.adoc[]') }
+  let(:partial_lines) { [':max-include-depth: 1000'] + Array.new(10, 'include::a.adoc[4..6]') }
 
   before do
     allow(project.repository).to receive(:blob_at).with(ref, 'a.adoc').and_return(a_blob)
@@ -46,6 +50,10 @@ RSpec.describe Gitlab::Asciidoc::IncludeProcessor do
       max_includes.times { processor.send(:read_blob, ref, 'a.adoc') }
 
       expect(processor.send(:include_allowed?, 'foo.adoc', reader)).to be_falsey
+    end
+
+    it 'allows the include of partial lines' do
+      expect(processor.send(:include_allowed?, 'foo.adoc', reader_lines)).to be_truthy
     end
   end
 end
