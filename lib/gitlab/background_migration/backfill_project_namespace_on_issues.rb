5162ac60ec24ef10b5041491d7ef1acee625de17
Alexandru Croitor <acroitor@gitlab.com>
2022-07-06 19:43:39 +0300

    Backfill namespace_id on issues table
    Backfill namespace_id for existing issues that should correspond to the corresponding
    issue.project.project_namespace_id
    
    Changelog: added

---
+++ lib/gitlab/background_migration/backfill_project_namespace_on_issues.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Gitlab
+  module BackgroundMigration
+    # Back-fills the `issues.namespace_id` by setting it to corresponding project.project_namespace_id
+    class BackfillProjectNamespaceOnIssues < BatchedMigrationJob
+      def perform
+        cleanup_gin_index('issues')
+
+        each_sub_batch(
+          operation_name: :update_all,
+          batching_scope: -> (relation) {
+            relation.joins("INNER JOIN projects ON projects.id = issues.project_id")
+              .select("projects.id, projects.project_namespace_id").where(issues: { namespace_id: nil })
+          }
+        ) do |sub_batch|
+          ApplicationRecord.connection.execute <<~SQL
+            UPDATE issues
+            SET namespace_id = projects.project_namespace_id
+            FROM (#{sub_batch.to_sql}) as projects(id, project_namespace_id)
+            WHERE project_id = projects.id
+          SQL
+        end
+      end
+
+      private
+
+      def cleanup_gin_index(table_name)
+        index_names = ApplicationRecord.connection.select_values <<~SQL
+          select indexname::text from pg_indexes where tablename = '#{table_name}' and indexdef ilike '%using gin%'
+        SQL
+
+        index_names.each do |index_name|
+          ApplicationRecord.connection.execute("select gin_clean_pending_list('#{index_name}')")
+        end
+      end
+    end
+  end
+end
